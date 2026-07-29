28f34dd0ba52c46085383f22b2e8bb0c264a8857
admin <admin@zzyzzy.local>
2026-07-29 09:26:26 +0800

    Initial commit

---
+++ teleprompter/teleprompter/teleprompterApp.swift
@@ -0,0 +1,17 @@
+//
+//  teleprompterApp.swift
+//  teleprompter
+//
+//  Created by admin on 2026/7/29.
+//
+
+import SwiftUI
+
+@main
+struct teleprompterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
